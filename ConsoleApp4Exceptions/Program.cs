93f7e152105115db5384e1d65b01b50f774cdc99
oscarcortez <oscarkortez@gmail.com>
2021-03-31 20:09:08 -0400

    Add project files.

---
+++ ConsoleApp4Exceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿using Models;
+using System;
+
+namespace ConsoleApp4Exceptions
+{
+    class Program
+    {
+        /// <summary>
+        /// Es un evento que ocurre durante la ejecucion de un programa y que interrumpe el flujo normal de la operacion
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            DivNumbers d = new DivNumbers();
+            d.Division(25, 0);
+            Console.ReadKey();
+        }
+
+        static double SafeDivision(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return x / y;
+        }
+
+
+    }
+}
---
+++ Models/DivNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Models
+{
+    public class DivNumbers
+    {
+        int result;
+        public DivNumbers()
+        {
+            result = 0;
+        }
+
+        public void Division(int num1, int num2)
+        {
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine($"Exception caught: {e}");                
+            }
+            finally
+            {
+                Console.WriteLine($"Result: {result}");
+            }
+        }
+    }
+}
